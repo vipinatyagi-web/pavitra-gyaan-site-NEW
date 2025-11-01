85916c0f0dbc400c39dba6ef4827a69d9b92a2c0
vipinatyagi-web <vipin.a.tyagi@gmail.com>
2025-11-01 02:41:33 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,2 @@
 import "../styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+export default function App({Component,pageProps}){return <Component {...pageProps}/>}
